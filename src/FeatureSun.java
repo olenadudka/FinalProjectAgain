a280599cc863af2309121a6f2d984b06165a8b3a
olenadudka <helen.lady2311@gmail.com>
2022-12-21 14:21:35 -0500

    the basic solar system with the feAture sun added is ready to be deployed

---
+++ src/FeatureSun.java
@@ -0,0 +1,6 @@
+public class FeatureSun {
+    String color;
+    String temperature;
+    String size;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+       solarSystem Xron=new solarSystem();
+       Xron.moon=2;
+       Xron.planets=2;
+       Xron.sun=1;
+
+        FeatureSun Fxron=new FeatureSun();
+        Fxron.color="green";
+        Fxron.size="1234455km";
+        Fxron.temperature="-23456678";
+
+    }
+}
